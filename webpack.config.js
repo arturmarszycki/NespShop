977c15ae8ae167a0e84deea546b0970144fc9085
Artur Marszycki <artur.marszycki@gmail.com>
2020-04-02 11:54:31 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const entryPath = "src";
+const entryFile = "index.js";
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    entry: ["whatwg-fetch", `./${entryPath}/${entryFile}`],
+    output: {
+        filename: "out.js",
+        path: path.resolve(__dirname, `${entryPath}/build`)
+    },
+    devServer: {
+        contentBase: path.join(__dirname, `${entryPath}`),
+        publicPath: "/build/",
+        compress: true,
+        port: 3001
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [autoprefixer()]
+                        }
+                    },
+                    'sass-loader'
+                ]
+            }
+        ]
+    }
+};
